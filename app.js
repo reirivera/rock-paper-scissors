b7a2a82d08e8f7cf32ba12bf7cede17f9b8eb2d7
diego <2019300@collegemv.qc.ca>
2022-08-01 12:48:44 -0400

    playround

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let userChoice = 'rock'
 
-function computerChoice (){
-  
+
+function computerMove (){
+    
     let move = Math.floor(Math.random() * 3);
 
     if(move === 0){
@@ -16,5 +16,27 @@ function computerChoice (){
 }
 
 
-
- console.log(computerChoice());
+function playRound (playerChoice, computerChoice){
+    if (playerChoice === 'rock' && computerChoice === 'scissors'){
+        console.log('YOU WON! ROCK BEATS SCISSORS.')
+    }else if (playerChoice === 'paper' && computerChoice === 'rock'){
+        console.log('YOU WON! PAPER BEATS ROCK.')
+    }else if(playerChoice === 'scissors' && computerChoice ==='paper'){
+        console.log('YOU WON! SCISSORS BEATS PAPER.')
+    }else if (computerChoice === 'rock' && playerChoice === 'scissors'){
+        console.log('YOU LOST! ROCK BEATS SCISSORS.')
+    }else if (computerChoice === 'paper' && playerChoice === 'rock'){
+        console.log('YOU LOST! PAPER BEATS ROCK.')
+    }else if(computerChoice === 'scissors' && playerChoice ==='paper'){
+        console.log('YOU LOST! SCISSORS BEATS PAPER.')
+    }else if(playerChoice === 'rock' && computerChoice === 'rock'){
+        console.log('ITS A TIE')
+    }else if(playerChoice === 'paper' && computerChoice === 'paper'){
+        console.log('ITS A TIE')
+    }else if(playerChoice === 'scissors' && computerChoice === 'scissors'){
+        console.log('ITS A TIE')
+    }
+}
+const playerSelection = 'rock';
+const computerSelection = computerMove();
+console.log(playRound(playerSelection, computerSelection));
